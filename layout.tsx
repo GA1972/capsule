b6704eba514ca505d0f5845470baa0274a16359d
GA1972 <gieve.acidwalla@gmail.com>
2025-08-10 02:37:08 +0530

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Capsule" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ fontFamily: "Inter, system-ui" }}>{children}</body>
-    </html>
-  );
-}
