e77ba4b99bd6f164dba6cd6a5b337ce2714ee4f8
GA1972 <gieve.acidwalla@gmail.com>
2025-08-10 02:36:45 +0530

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 32 }}>
-      <h1>Capsule</h1>
-      <p>Your meeting recorder cum AI notemaker.</p>
-      <a href="/auth">Sign in with Google</a>
-    </main>
-  );
-}
